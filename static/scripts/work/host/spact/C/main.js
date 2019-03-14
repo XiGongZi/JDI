f7cea5ea4a993b393c9240e294ad62fe04bb975e
xigongzi <1079688386@qq.com>
2019-03-14 17:34:22 +0800

    某修改

--- static/scripts/work/host/spact/C/main.js
+++ static/scripts/work/host/spact/C/main.js
@@ -93,8 +93,6 @@ define([
                                 
                                 </div>
                             </div>
-            
-            
                     </div>
             </div>
                     `
